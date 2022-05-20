36a560e28230980ac5b98f1b4ff64e18c885e88e
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-05-20 13:36:11 +0300

    Bcrypt hash password

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
@@ -26,5 +27,34 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+/**
+ * While saving user, we need to make sure the password is hashed, not plain password
+ * hashing should be done only in 2 situations
+ * 1. if it is the first time a user is being saved/created
+ * 2. user have updated/modified the existing password
+ * for handling such requirements, we can use 'pre' middleware in our schema
+ * this middleware/function will run each time user is saved/created
+ * and/or password is modified/updated
+ */
+
+userSchema.pre("save", function (next) {
+    let user = this;
+    // hash password only if user is changing the password or registering for the first time
+    // make sure to use this otherwise each time user.save() is executed, password
+    // will get auto updated and you can't login with original password
+    if (user.isModified("password")) {
+      return bcrypt.hash(user.password, 12, function (err, hash) {
+        if (err) {
+          console.log("BCRYPT HASH ERR ", err);
+          return next(err);
+        }
+        user.password = hash;
+        return next();
+      });
+    } else {
+      return next();
+    }
+  });
 
 export default mongoose.model("User", userSchema);
