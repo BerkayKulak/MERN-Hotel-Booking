c2b80722ca9f181989b0e66d1fabcd97ed89cb1a
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-05-20 15:52:23 +0300

    Process login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { showMessage,register } from "../controllers/auth";
+import { showMessage, register, login } from "../controllers/auth";
 
 const router = express.Router();
 
 router.get("/:message", showMessage);
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
